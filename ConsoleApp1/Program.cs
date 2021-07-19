8c478bc88615a609dc01ca9fa565c8502e01ac4b
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-07-19 10:58:14 +0530

    add : addition and substraction program uploaded

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int addition = num1 + num2;
+            int subtraction = num1 - num2;
+
+            Console.Write(" Addition : " + addition);
+            Console.Write(" Subtraction : " + subtraction);
+
+        }
+    }
+}
